d95d302e40a9cdbb4e82186e825bb793b6054c52
Aman Garg <amangrg96@gmail.com>
2023-12-26 12:16:43 +0530

    feat: vercel analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Document() {
   const meta = {
@@ -30,6 +31,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <SpeedInsights />
+        <Analytics />
       </body>
     </Html>
   )
