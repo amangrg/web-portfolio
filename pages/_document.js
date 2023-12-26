d28c9c58fd3de6a8f99564e852b07e79dc44d54d
Aman Garg <amangrg96@gmail.com>
2023-12-26 18:30:28 +0530

    feat: try fixing vercel analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import { Analytics } from '@vercel/analytics/react'
 
 export default function Document() {
   const meta = {
@@ -30,8 +28,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <SpeedInsights />
-        <Analytics />
       </body>
     </Html>
   )
