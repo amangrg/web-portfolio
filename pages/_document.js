89ac040b9d2513ac194fde7e3848865604256709
Aman Garg <amangrg96@gmail.com>
2023-12-26 11:53:32 +0530

    feat: add sharp for image optimisation

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: "Aman Garg's writing, artwork and web development portfolio",
+    title:
+      "Aman Garg's writing, artwork, blog posts and web development portfolio",
     description:
-      'Digital artwork, poetry, short stories and blog posts. Also web dev projects',
+      'Digital artwork, poetry, short stories and whatever I find interesting. Also web dev projects',
     image:
       'https://www.amangarg.in/_next/image?url=%2Fimages%2Fart-1.png&w=3840&q=75'
   }
